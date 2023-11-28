ff90c6fcc84b242bf9b1b10e094e8fbdf2e67e13
hur2 <dhwoghks0626@naver.com>
2023-11-28 15:48:00 +0900

    add flask_cors for cors problem

--- server/app.py
+++ server/app.py
@@ -1,8 +1,9 @@
 from flask import Flask, request
+from flask_cors import CORS
 from funcs import *
 
-
 app = Flask(__name__)
+CORS(app, resources={r'*': {'origins': "http://localhost:3000"}})
 
 @app.route('/')
 def home():
